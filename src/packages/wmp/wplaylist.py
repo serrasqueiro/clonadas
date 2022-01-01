3bae0ae412e3f6ee749c1f6da4ea16a93b6e85e4
Henrique Moreira <h@serrasqueiro.com>
2022-01-01 19:44:17 +0000

    basic fit

--- src/packages/wmp/wplaylist.py
+++ src/packages/wmp/wplaylist.py
@@ -6,23 +6,29 @@
 
 # pylint: disable=missing-function-docstring, no-self-use
 
-from lxml import etree 
+from lxml import etree
 
 
 class Playlist():
     """ Win. M. Player playlist
     """
     _adir = ""
+    _resume = None
 
     def __init__(self, where=None):
+        self._resume = None
         if where is None:
             self._root = None
         else:
             self._root = self._from_file(where)
 
     def get_xml(self):
-        assert self._root
-        return self._root
+        assert self._root is not None
+        axml = self._root
+        self._resume = [
+            (idx, [ala.tag for ala in item]) for idx, item in enumerate(axml, start=1)
+        ]
+        return axml
 
     def _from_file(self, where:str):
         with open(where, "r", encoding="utf-8") as fdin:
@@ -34,3 +40,4 @@ class Playlist():
 # Main script
 if __name__ == "__main__":
     print("Please import me!")
+    #pls = wmp.wplaylist.Playlist(wmp.wmplay.WMPlay().last_played())
