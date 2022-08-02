f4d24565b6efc9f55b4b7a79f80a601493af1efc
Henrique Moreira <h@serrasqueiro.com>
2022-08-02 16:27:34 +0100

    hint!

--- src/packages/zson/__init__.py
+++ src/packages/zson/__init__.py
@@ -2,6 +2,10 @@
 
 import zson.zobject
 import zson.idtable
+import zson.csv
+
+# Hint:
+#	[(key, val) for key, val in sys.modules.items() if "zson" in key]
 
 # zson module - (c)2021, 2022  Henrique Moreira
 MODULE_VERSION = "1.00 14"
